dd3a57021ad1162ea42544dbe035afb2ee4966cb
Nico Paulino <nicopaulino8@gmail.com>
2022-03-22 17:27:29 -0500

    Add Encouragement Page

---
+++ src/Encouragement.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, {useState} from "react";
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom'
+
+
+function Encouragement() {
+  
+
+  return (
+    <div>BE ENCOURAGED</div>
+  );
+}
+
+export default Encouragement;
